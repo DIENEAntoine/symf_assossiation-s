b652e3270cdb176dfb603eda7e98b7cdccca5e45
DIENEAntoine <diene1antoine@gmail.com>
2023-02-22 17:00:40 +0100

    Système d'authentification fait

---
+++ src/Trait/TimeZone.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Trait;
+   
+    Trait TimeZone
+    {
+        public function changeTimeZone($time_zone)
+        {
+            date_default_timezone_set($time_zone);
+        }
+    }
+    
